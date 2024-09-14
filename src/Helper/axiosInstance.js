d24a2a5de8343e66db46b9d17c377a058c23b311
ChinmayKaitade <chinmaykaitade123@gmail.com>
2024-09-14 16:43:44 +0530

    client added

---
+++ src/Helper/axiosInstance.js
@@ -0,0 +1,10 @@
+import axios from "axios";
+
+const BASE_URL = "https://lms-frontend-ij4j.onrender.com/api/v1";
+
+const axiosInstance = axios.create();
+
+axiosInstance.defaults.baseURL = BASE_URL;
+axiosInstance.defaults.withCredentials = true;
+
+export default axiosInstance;
