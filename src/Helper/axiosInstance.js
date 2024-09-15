6a621ceb2996e25e93dc260cd0416dbfc085842e
ChinmayKaitade <chinmaykaitade123@gmail.com>
2024-09-15 07:13:14 +0530

    baseurl error fixed

--- src/Helper/axiosInstance.js
+++ src/Helper/axiosInstance.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "https://lms-frontend-ij4j.onrender.com/api/v1";
+const BASE_URL = "https://lms-server-7sca.onrender.com/api/v1";
 
 const axiosInstance = axios.create();
 
